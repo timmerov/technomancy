b1e9e85d32fc77b8df7a87578002f7b2260c7cf5
timmer <timmer@castar.com>
2016-10-21 17:28:43 -0700

    pass texture coordinates to shaders.

--- world/src/render.cc
+++ world/src/render.cc
@@ -22,17 +22,21 @@ namespace {
     auto g_vertex_source =R"shader_code(
         #version 310 es
         layout (location = 0) in vec3 vertex_pos_in;
+        layout (location = 1) in vec2 texture_pos_in;
+        out mediump vec2 texture_pos;
         uniform mat4 proj_view_mat;
         void main() {
             gl_Position = proj_view_mat * vec4(vertex_pos_in, 1.0f);
+            texture_pos = texture_pos_in;
         }
     )shader_code";
 
     auto g_fragment_source = R"shader_code(
         #version 310 es
+        in mediump vec2 texture_pos;
         out mediump vec4 frag_color;
         void main() {
-            frag_color = vec4(0.0f, 0.8f, 0.8f, 1.0f);
+            frag_color = vec4(texture_pos.x, texture_pos.y, 0.0f, 1.0f);
         }
     )shader_code";
 
@@ -44,7 +48,9 @@ namespace {
         int width_ = 0;
         int height_ = 0;
         int num_triangles_ = 0;
+        GLuint vertex_id_ = 0;
         GLuint vertex_buffer_ = 0;
+        GLuint texture_buffer_ = 0;
         GLuint index_buffer_ = 0;
         GLuint vertex_shader_ = 0;
         GLuint fragment_shader_ = 0;
@@ -66,13 +72,19 @@ namespace {
             }
             LOG("num_vertexes=" << sphere.num_vertexes_ << " num_faces=" << sphere.num_faces_);
 
-            int num_floats = 3 * sphere.num_vertexes_;
-            auto vertex_array = new(std::nothrow) GLfloat[num_floats];
+            int num_vertex_floats = 3 * sphere.num_vertexes_;
+            auto vertex_array = new(std::nothrow) GLfloat[num_vertex_floats];
             for (int i = 0; i < sphere.num_vertexes_; ++i) {
                 vertex_array[3*i+0] = (GLfloat) sphere.vertex_[i].x_;
                 vertex_array[3*i+1] = (GLfloat) sphere.vertex_[i].y_;
                 vertex_array[3*i+2] = (GLfloat) sphere.vertex_[i].z_;
                 //LOG("vertex[" << i << "]={" << vertex_array_[3*i+0] << ", " << vertex_array_[3*i+1] << ", " << vertex_array_[3*i+2] << "}");
+            }
+            int num_texture_floats = 2 * sphere.num_vertexes_;
+            auto texture_array = new(std::nothrow) GLfloat[num_texture_floats];
+            for (int i = 0; i < sphere.num_vertexes_; ++i) {
+                texture_array[2*i+0] = (GLfloat) sphere.texture_[i].x_;
+                texture_array[2*i+1] = (GLfloat) sphere.texture_[i].y_;
             }
             num_indexes_ = 3 * sphere.num_faces_;
             auto index_array = new(std::nothrow) GLushort[num_indexes_];
@@ -89,9 +101,17 @@ namespace {
             glDepthFunc(GL_LESS);
             glDisable(GL_CULL_FACE);
 
+            glGenVertexArrays(1, &vertex_id_);
+            glBindVertexArray(vertex_id_);
+
             glGenBuffers(1, &vertex_buffer_);
             glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer_);
-            glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat)*num_floats, vertex_array, GL_STATIC_DRAW);
+            glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat)*num_vertex_floats, vertex_array, GL_STATIC_DRAW);
+            LOG("vertex=" << vertex_buffer_);
+
+            glGenBuffers(1, &texture_buffer_);
+            glBindBuffer(GL_ARRAY_BUFFER, texture_buffer_);
+            glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat)*num_texture_floats, texture_array, GL_STATIC_DRAW);
             LOG("vertex=" << vertex_buffer_);
 
             glGenBuffers(1, &index_buffer_);
@@ -126,6 +146,7 @@ namespace {
             LOG("proj_view_mat_loc=" << proj_view_mat_loc_);
 
             delete[] index_array;
+            delete[] texture_array;
             delete[] vertex_array;
         }
 
@@ -152,9 +173,17 @@ namespace {
                 glDeleteBuffers(1, &index_buffer_);
                 index_buffer_ = 0;
             }
+            if (texture_buffer_) {
+                glDeleteBuffers(1, &texture_buffer_);
+                texture_buffer_ = 0;
+            }
             if (vertex_buffer_) {
                 glDeleteBuffers(1, &vertex_buffer_);
                 vertex_buffer_ = 0;
+            }
+            if (vertex_id_) {
+                glDeleteVertexArrays(1, &vertex_id_);
+                vertex_id_ = 0;
             }
         }
 
@@ -178,9 +207,15 @@ namespace {
             glUseProgram(program_);
             glUniformMatrix4fv(proj_view_mat_loc_, 1, GL_FALSE, &proj_view_mat[0][0]);
 
+            glBindVertexArray(vertex_id_);
+
             glEnableVertexAttribArray(0);
             glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer_);
             glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+
+            glEnableVertexAttribArray(1);
+            glBindBuffer(GL_ARRAY_BUFFER, texture_buffer_);
+            glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, nullptr);
 
             glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, index_buffer_);
 
@@ -188,7 +223,9 @@ namespace {
 
             glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
             glBindBuffer(GL_ARRAY_BUFFER, 0);
+            glDisableVertexAttribArray(1);
             glDisableVertexAttribArray(0);
+            glBindVertexArray(0);
             glUseProgram(0);
         }
 
