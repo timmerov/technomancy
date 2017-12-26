7d498a98d6356a75c82535e07e0b4a27f00d2a3d
timmer <timmerov+github@gmail.com>
2017-12-26 12:54:29 -0800

    move pluto and charon around each other.

--- pluto/src/main.cc
+++ pluto/src/main.cc
@@ -15,16 +15,31 @@ pluto simulation.
 
 namespace {
 /**
+double is 64 bits (8 bytes)
+long double is 128 bits (16 bytes)
+long double is much slower and no more accurate.
+weird.
+**/
+//typedef long double fp;
+typedef double fp;
+
+/**
+configurations
+**/
+	const int kYears = 1;
+	const int kTimeIntervals = 10*1000*1000;
+
+/**
 numbers from wikipedia
 **/
-	const double kPlutoRadiusKm = 1188.3;
-	const double kPlutoMassKg = 1.303e22;
-	const double kCharonRadiusKm = 606.0;
-	const double kCharonMassKg =1.586e21;
-	const double kSeparationKm = 19571;
-	const double kCharonBarycenterKm = 17536;
-	const double kPeriodDays = 6.3872304;
-	const double kBigGM3pKgpS = 6.67408e-11;
+	const fp kPlutoRadiusKm = 1188.3;
+	const fp kPlutoMassKg = 1.303e22;
+	const fp kCharonRadiusKm = 606.0;
+	const fp kCharonMassKg =1.586e21;
+	const fp kSeparationKm = 19571;
+	const fp kCharonBarycenterKm = 17536;
+	const fp kPeriodDays = 6.3872304;
+	const fp kBigGM3pKgpS = 6.67408e-11;
 
 /**
 definitions:
@@ -48,7 +63,25 @@ formulas:
 	:   centripetal acceleration of m2:  v2^2 / r2
 	:                   velocity of m1:  (G * m2 / r^2 * r2)^0.5
 	:                   velocity of m2:  (G * m1 / r^2 * r1)^0.5
+
+update formulas:
+	: position: x += v*dt + 0.5*a*dt^2
+	: velocity: v += a*dt
 **/
+
+	class Vector2 {
+	public:
+		fp x_;
+		fp y_;
+	};
+
+	class Object {
+	public:
+		Vector2 pos_;
+		Vector2 vel_;
+		Vector2 acc_;
+		fp gm_;
+	};
 
     class Pluto {
     public:
@@ -56,26 +89,38 @@ formulas:
         Pluto(const Pluto &) = default;
         ~Pluto() = default;
 
+		fp pi_ = 3.14;
+		Object pluto_;
+		Object charon_;
+		fp periodS_ = 0.0;
+
         void run() noexcept {
 			std::cout << "Hello, World!" << std::endl;
 
-			double pi = std::acos(-1.0);
+			pi_ = std::acos(-1.0);
 
-			double p_volume = 4.0/3.0*pi*kPlutoRadiusKm*kPlutoRadiusKm*kPlutoRadiusKm;
-			double c_volume = 4.0/3.0*pi*kCharonRadiusKm*kCharonRadiusKm*kCharonRadiusKm;
-			double p_density = kPlutoMassKg / p_volume;
-			double c_density = kCharonMassKg / c_volume;
-			double avg_density = (p_density + c_density) / 2.0;
+			setup();
+			process();
 
-			double pc_radius = kPlutoRadiusKm / kCharonRadiusKm;
-			double pc_mass = kPlutoMassKg / kCharonMassKg;
+			std::cout << "Goodbye, World!" << std::endl;
+        }
 
-			double pc_sep_p_radius = kSeparationKm / kPlutoRadiusKm;
-			double pc_sep_c_radius = kSeparationKm / kCharonRadiusKm;
+        void setup() noexcept {
+			fp p_volume = 4.0/3.0*pi_*kPlutoRadiusKm*kPlutoRadiusKm*kPlutoRadiusKm;
+			fp c_volume = 4.0/3.0*pi_*kCharonRadiusKm*kCharonRadiusKm*kCharonRadiusKm;
+			fp p_density = kPlutoMassKg / p_volume;
+			fp c_density = kCharonMassKg / c_volume;
+			fp avg_density = (p_density + c_density) / 2.0;
 
-			double period_hours = kPeriodDays * 24.0;
-			double period_minutes = period_hours * 60.0;
-			double period_seconds = period_minutes * 60.0;
+			fp pc_radius = kPlutoRadiusKm / kCharonRadiusKm;
+			fp pc_mass = kPlutoMassKg / kCharonMassKg;
+
+			fp pc_sep_p_radius = kSeparationKm / kPlutoRadiusKm;
+			fp pc_sep_c_radius = kSeparationKm / kCharonRadiusKm;
+
+			fp period_hours = kPeriodDays * 24.0;
+			fp period_minutes = period_hours * 60.0;
+			fp period_seconds = period_minutes * 60.0;
 
 			std::cout << "Pluto   Radius : " << kPlutoRadiusKm << " km" << std::endl;
 			std::cout << "Pluto   Mass   : " << kPlutoMassKg << " kg" << std::endl;
@@ -88,6 +133,7 @@ formulas:
 			std::cout << "P/C     Radius : " << pc_radius << std::endl;
 			std::cout << "P/C     Mass   : " << pc_mass << std::endl;
 			std::cout << "Average Density: " << avg_density << " kg/km^3" << std::endl;
+			std::cout << "Separation             : " << kSeparationKm << " km" << std::endl;
 			std::cout << "Separation Pluto  Radii: " << pc_sep_p_radius << std::endl;
 			std::cout << "Separation Charon Radii: " << pc_sep_c_radius << std::endl;
 			std::cout << "Period: " << kPeriodDays << " days" << std::endl;
@@ -95,19 +141,14 @@ formulas:
 			std::cout << "Period: " << period_minutes << " minutes" << std::endl;
 			std::cout << "Period: " << period_seconds << " seconds" << std::endl;
 
-			double m1 = kPlutoMassKg;
-			double m2 = kCharonMassKg;
-			double m = m1 + m2;
-			double r = kSeparationKm;
-			double r1 = r * m2 / m;
-			double r2 = r * m1 / m;
-			double G = kBigGM3pKgpS / 1e9; /// units
-			double g1 = G * m2 / (r * r);
-			double g2 = G * m1 / (r * r);
-			double v1 = std::sqrt(g1 * r1);
-			double v2 = std::sqrt(g2 * r2);
-			double T1 = 2 * pi * r1 / v1;
-			double T2 = 2 * pi * r2 / v2;
+			init_objects();
+
+			fp r1 = pluto_.pos_.y_;
+			fp r2 = - charon_.pos_.y_;
+			fp v1 = - pluto_.vel_.x_;
+			fp v2 = charon_.vel_.x_;
+			fp T1 = 2 * pi_ * r1 / v1;
+			fp T2 = 2 * pi_ * r2 / v2;
 
 			std::cout << "r1: " << r1 << " km" << std::endl;
 			std::cout << "r2: " << r2 << " km" << std::endl;
@@ -116,8 +157,123 @@ formulas:
 			std::cout << "T1: " << T1 << " s" << std::endl;
 			std::cout << "T2: " << T2 << " s" << std::endl;
 
-			std::cout << "Goodbye, World!" << std::endl;
+			periodS_ = T1;
         }
+
+        void init_objects() noexcept {
+			fp m1 = kPlutoMassKg;
+			fp m2 = kCharonMassKg;
+			fp m = m1 + m2;
+			fp r = kSeparationKm;
+			fp r1 = r * m2 / m;
+			fp r2 = r * m1 / m;
+			fp G = kBigGM3pKgpS / 1e9; /// units
+			fp gm1 = G * m2;
+			fp gm2 = G * m1;
+			fp g1 = gm1 / (r * r);
+			fp g2 = gm2 / (r * r);
+			fp v1 = std::sqrt(g1 * r1);
+			fp v2 = std::sqrt(g2 * r2);
+
+			pluto_.pos_.x_ = 0.0;
+			pluto_.pos_.y_ = r1;
+			pluto_.vel_.x_ = - v1;
+			pluto_.vel_.y_ = 0.0;
+			pluto_.acc_.x_ = 0.0;
+			pluto_.acc_.y_ = - g1;
+			pluto_.gm_ = gm1;
+
+			charon_.pos_.x_ = 0.0;
+			charon_.pos_.y_ = - r2;
+			charon_.vel_.x_ = v2;
+			charon_.vel_.y_ = 0.0;
+			charon_.acc_.x_ = 0.0;
+			charon_.acc_.y_ = g2;
+			charon_.gm_ = gm2;
+        }
+
+        void process() noexcept {
+			fp dt = periodS_ / fp(kTimeIntervals);
+
+			print_objects();
+			fp e0 = print_energy();
+
+			for (int k = 0; k < kYears; ++k) {
+				for (int i = 0; i < kTimeIntervals; ++i) {
+					update_object(pluto_, dt);
+					update_object(charon_, dt);
+					update_gravity();
+				}
+
+				print_objects();
+				fp e1 = print_energy();
+				fp err = (e1 - e0) / e0;
+				std::cout << "Energy Drift: " << err << std::endl;
+			}
+        }
+
+        void print_objects() noexcept {
+			std::cout << "Pluto .pos.x=" << pluto_.pos_.x_ << " km" << std::endl;
+			std::cout << "Pluto .pos.y=" << pluto_.pos_.y_ << " km" << std::endl;
+			std::cout << "Pluto .vel.x=" << pluto_.vel_.x_ << " km/s" << std::endl;
+			std::cout << "Pluto .vel.y=" << pluto_.vel_.y_ << " km/s" << std::endl;
+			std::cout << "Pluto .acc.x=" << pluto_.acc_.x_ << " km/s^2" << std::endl;
+			std::cout << "Pluto .acc.y=" << pluto_.acc_.y_ << " km/s^2" << std::endl;
+			std::cout << "Charon.pos.x=" << charon_.pos_.x_ << " km" << std::endl;
+			std::cout << "Charon.pos.y=" << charon_.pos_.y_ << " km" << std::endl;
+			std::cout << "Charon.vel.x=" << charon_.vel_.x_ << " km/s" << std::endl;
+			std::cout << "Charon.vel.y=" << charon_.vel_.y_ << " km/s" << std::endl;
+			std::cout << "Charon.acc.x=" << charon_.acc_.x_ << " km/s^2" << std::endl;
+			std::cout << "Charon.acc.y=" << charon_.acc_.y_ << " km/s^2" << std::endl;
+        }
+
+        fp print_energy() noexcept {
+			fp m1 = kPlutoMassKg;
+			fp m2 = kCharonMassKg;
+			fp v1_2 = pluto_.vel_.x_*pluto_.vel_.x_ + pluto_.vel_.y_*pluto_.vel_.y_;
+			fp v2_2 = charon_.vel_.x_*charon_.vel_.x_ + charon_.vel_.y_*charon_.vel_.y_;
+            fp ke1 = 0.5 * m1 * v1_2;
+            fp ke2 = 0.5 * m2 * v2_2;
+			fp G = kBigGM3pKgpS / 1e9; /// units
+			fp dx = pluto_.pos_.x_ - charon_.pos_.x_;
+			fp dy = pluto_.pos_.y_ - charon_.pos_.y_;
+            fp r2 = dx*dx + dy*dy;
+			fp r = std::sqrt(r2);
+            fp pe = - G * m1 * m2 / r;
+            fp energy = ke1 + ke2 + pe;
+            std::cout << "Pluto  Kinetic Energy: " << ke1 << std::endl;
+            std::cout << "Charon Kinetic Energy: " << ke2 << std::endl;
+            std::cout << "Potential Energy     : " << pe << std::endl;
+            std::cout << "Total Energy         : " << energy << std::endl;
+            return energy;
+        }
+
+        void update_object(
+			Object& obj,
+			fp dt
+		) noexcept {
+			fp dvx = obj.acc_.x_*dt;
+			fp dvy = obj.acc_.y_*dt;
+            obj.pos_.x_ += (obj.vel_.x_ + 0.5*dvx) * dt;
+            obj.pos_.y_ += (obj.vel_.y_ + 0.5*dvy) * dt;
+            obj.vel_.x_ += dvx;
+            obj.vel_.y_ += dvy;
+		}
+
+		void update_gravity() noexcept {
+            fp dx = pluto_.pos_.x_ - charon_.pos_.x_;
+            fp dy = pluto_.pos_.y_ - charon_.pos_.y_;
+            fp r2 = dx*dx + dy*dy;
+			fp r = std::sqrt(r2);
+            fp g1 = pluto_.gm_ / r2;
+            fp g2 = charon_.gm_ / r2;
+            fp dxr = dx / r;
+            fp dyr = dy / r;
+            pluto_.acc_.x_ = - g1 * dxr;
+            pluto_.acc_.y_ = - g1 * dyr;
+            charon_.acc_.x_ = g2 * dxr;
+            charon_.acc_.y_ = g2 * dyr;
+		}
     };
 }
 
