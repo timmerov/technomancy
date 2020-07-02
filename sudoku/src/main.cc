485d76450d9f1d58039d4aab3b0cddbccde9b418
Tim Cotter <timmerov+github@gmail.com>
2020-07-02 11:05:48 -0700

    greedy solve sudoku.

--- sudoku/src/main.cc
+++ sudoku/src/main.cc
@@ -26,16 +26,57 @@ specifically, this sudoku:
 #include <aggiornamento/aggiornamento.h>
 #include <aggiornamento/log.h>
 
+#include <iomanip>
 #include <sstream>
 #include <vector>
 
 namespace {
 
+class Move {
+public:
+    int row_;
+    int col1_;
+    int col2_;
+};
+using Moves = std::vector<Move>;
+
 class Sudoku {
 public:
     int board_[9][9];
+    Moves moves_;
+
+    void run() noexcept {
+        init();
+        print_board();
+        float old_cost = evaluate();
+        for (int i = 0; i < 1000*1000; ++i) {
+            int move = choose_move();
+            swap_move(move);
+            float new_cost = evaluate();
+            if (new_cost < old_cost) {
+                old_cost = new_cost;
+                LOG("cost: "<<std::fixed<<std::setprecision(3)<<old_cost);
+                if (old_cost == 0.0) {
+                    break;
+                }
+            } else {
+                swap_move(move);
+            }
+        }
+        print_board();
+    }
 
     void init() noexcept {
+        std::srand(std::time(nullptr));
+
+        init_board_moves();
+
+        int nmoves = moves_.size();
+        LOG("number of moves: "<<nmoves);
+    }
+
+    void init_board_moves() noexcept {
+        moves_.clear();
         init_row(0, {0, 0, 0,  0, 0, 0,  0, 6, 0});
         init_row(1, {2, 8, 0,  0, 0, 0,  0, 0, 4});
         init_row(2, {0, 0, 7,  0, 0, 5,  8, 0, 0});
@@ -45,7 +86,6 @@ public:
         init_row(6, {0, 0, 5,  1, 0, 0,  2, 0, 0});
         init_row(7, {3, 0, 0,  0, 0, 0,  0, 8, 1});
         init_row(8, {0, 9, 0,  0, 0, 0,  0, 0, 0});
-        print_board();
     }
 
     void init_row(
@@ -85,6 +125,17 @@ public:
                 ++idx;
             }
         }
+        for (int i = 0; i < 9; ++i) {
+            if (vec[i] != 0) {
+                continue;
+            }
+            for (int k = i+1; k < 9; ++k) {
+                if (vec[k] != 0) {
+                    continue;
+                }
+                moves_.push_back({row, i, k});
+            }
+        }
     }
 
     void print_board() noexcept {
@@ -122,9 +173,81 @@ public:
         std::cout<<std::endl;
     }
 
-    void run() noexcept {
-        LOG("Hello, World!");
-        LOG("Goodbye, World!");
+    float evaluate() noexcept {
+        int cost = 0;
+        int count[10];
+        count[0] = 0;
+        for (int col = 0; col < 9; ++col) {
+            for (int i = 1; i <= 9; ++i) {
+                count[i] = 0;
+            }
+            for (int row = 0; row < 9; ++row) {
+                int x = board_[row][col];
+                ++count[x];
+            }
+            for (int i = 1; i <= 9; ++i) {
+                if (count[i] == 0) {
+                    ++cost;
+                }
+            }
+        }
+        for (int block_row = 0; block_row < 9; block_row += 3) {
+            for (int block_col = 0; block_col < 9; block_col += 3) {
+                for (int i = 1; i <= 9; ++i) {
+                    count[i] = 0;
+                }
+                for (int row = 0; row < 3; ++row) {
+                    int r = block_row + row;
+                    for (int col = 0; col < 3; ++col) {
+                        int c = block_col + col;
+                        int x = board_[r][c];
+                        ++count[x];
+                    }
+                }
+                for (int i = 1; i <= 9; ++i) {
+                    if (count[i] == 0) {
+                        ++cost;
+                    }
+                }
+            }
+        }
+
+        /**
+        what's the worst possible cost?
+        123|456|789
+        123|456|789
+        123|456|789
+        ---+---+---
+        123|456|789
+        123|456|789
+        123|456|789
+        ---+---+---
+        123|456|789
+        123|456|789
+        123|456|789
+        every column has cost 8.
+        every block has cost 6.
+        so 8*9 + 6*9 = 126
+        **/
+        float scaled = float(cost) / 126.0;
+        return scaled;
+    }
+
+    int choose_move() noexcept {
+        int nmoves = moves_.size();
+        for(;;) {
+            int x = 1 + std::rand()/((RAND_MAX + 1u)/nmoves);
+            if (x < nmoves) {
+                return x;
+            }
+        }
+    }
+
+    void swap_move(
+        int idx
+    ) noexcept {
+        auto& move = moves_[idx];
+        std::swap(board_[move.row_][move.col1_], board_[move.row_][move.col2_]);
     }
 };
 
@@ -139,7 +262,6 @@ int main(
     agm::log::init(AGM_TARGET_NAME ".log");
 
     Sudoku s;
-    s.init();
     s.run();
 
     return 0;
