ac81d0bd72bd8671238fce6d8817b19c26c37412
timmer <timmerov+git@gmail.com>
2025-10-31 12:18:25 -0700

    abandon solving sudokus by annealing. initialize regions.

--- sudoku/src/main.cc
+++ sudoku/src/main.cc
@@ -1,9 +1,9 @@
 /*
-Copyright (C) 2012-2020 tim cotter. All rights reserved.
+Copyright (C) 2012-2025 tim cotter. All rights reserved.
 */
 
 /**
-solve sudoku by simulated annealing.
+solve sudokus
 
 specifically, this sudoku:
 
@@ -21,171 +21,189 @@ specifically, this sudoku:
 | . 9 . | . . . | . . . |
 +-------+-------+-------+
 
+row is 1x9 cells.
+column is 9x1 cells.
+box is 3x3 cells.
+a region is a row, column, or box.
+cells must contain 0 or a unique digit.
+
 **/
 
 #include <aggiornamento/aggiornamento.h>
 #include <aggiornamento/log.h>
 
-#include <chrono>
 #include <iomanip>
-#include <random>
 #include <sstream>
 #include <vector>
 
+
 namespace {
 
-class Move {
+class Region {
 public:
-    int row_;
-    int col1_;
-    int col2_;
+    std::string name_;
+    int cells[9];
 };
-using Moves = std::vector<Move>;
+
+typedef std::vector<Region> Regions;
 
 class Sudoku {
 public:
-    int board_[9][9];
-    Moves moves_;
-    std::mt19937_64 rng_;
-    std::uniform_real_distribution<double> unif_;
+    int board_[81];
+    Regions regions_;
 
     void run() noexcept {
         init();
         print_board();
-
-        double max_diff = 0.0;
-        double sum_diff = 0.0;
-        int count_diffs = 0;
-
-        double best_cost = 1.0;
-        double old_cost = evaluate();
-        static const int kIterations = 1000*1000;
-        for (int i = 0; i < kIterations; ++i) {
-            int move = choose_move();
-            swap_move(move);
-            double new_cost = evaluate();
-            bool keep_it = false;
-            if (new_cost < old_cost) {
-                keep_it = true;
-            } else {
-                /**
-                the maximum possible difference is about 0.03.
-                temperatures higher than that are basically shuffling the board.
-                **/
-                double temp = 1.0 - double(i) / double(kIterations);
-                temp *= temp;
-                temp *= temp;
-                temp *= 0.03;
-                double delta = old_cost - new_cost;
-                double prob = std::exp(delta/temp);
-                //std::cout<<"=TSC= temp="<<temp<<" prob="<<prob<<" costs: "<<old_cost<<" "<<new_cost<<std::endl;
-                double r = unif_(rng_);
-                if (r < prob) {
-                    keep_it = true;
-                }
-
-                delta = std::abs(delta);
-                if (max_diff < delta) {
-                    max_diff = delta;
-                }
-                sum_diff += delta;
-                ++count_diffs;
-            }
-            if (keep_it) {
-                old_cost = new_cost;
-                if (best_cost > old_cost) {
-                    best_cost = new_cost;
-                    LOG(i<<": cost: "<<std::fixed<<std::setprecision(3)<<best_cost);
-                }
-                if (old_cost == 0.0) {
-                    break;
-                }
-            } else {
-                swap_move(move);
-            }
-        }
-        print_board();
-
-        double avg_diff = sum_diff / double(count_diffs);
-        LOG("cost difference max: "<<max_diff<<" avg: "<<avg_diff);
     }
 
     void init() noexcept {
-        std::srand(std::time(nullptr));
-        auto now = std::chrono::high_resolution_clock::now();
-        std::uint64_t seed = now.time_since_epoch().count();
-        std::uint64_t seed_lo = seed & 0xFFFFFFFF;
-        std::uint64_t seed_hi = seed >> 32;
-        std::seed_seq ss{seed_lo, seed_hi};
-        rng_.seed(ss);
-        unif_ = std::uniform_real_distribution<double>(0, 1);
-
-        init_board_moves();
-
-        int nmoves = moves_.size();
-        LOG("number of moves: "<<nmoves);
+        init_board_row(0, {0, 0, 0,  0, 0, 0,  0, 6, 0});
+        init_board_row(1, {2, 8, 0,  0, 0, 0,  0, 0, 4});
+        init_board_row(2, {0, 0, 7,  0, 0, 5,  8, 0, 0});
+        init_board_row(3, {5, 0, 0,  3, 4, 0,  0, 2, 0});
+        init_board_row(4, {4, 0, 0,  5, 0, 1,  0, 0, 8});
+        init_board_row(5, {0, 1, 0,  0, 7, 6,  0, 0, 3});
+        init_board_row(6, {0, 0, 5,  1, 0, 0,  2, 0, 0});
+        init_board_row(7, {3, 0, 0,  0, 0, 0,  0, 8, 1});
+        init_board_row(8, {0, 9, 0,  0, 0, 0,  0, 0, 0});
+        init_regions();
+        check_regions();
     }
 
-    void init_board_moves() noexcept {
-        moves_.clear();
-        init_row(0, {0, 0, 0,  0, 0, 0,  0, 6, 0});
-        init_row(1, {2, 8, 0,  0, 0, 0,  0, 0, 4});
-        init_row(2, {0, 0, 7,  0, 0, 5,  8, 0, 0});
-        init_row(3, {5, 0, 0,  3, 4, 0,  0, 2, 0});
-        init_row(4, {4, 0, 0,  5, 0, 1,  0, 0, 8});
-        init_row(5, {0, 1, 0,  0, 7, 6,  0, 0, 3});
-        init_row(6, {0, 0, 5,  1, 0, 0,  2, 0, 0});
-        init_row(7, {3, 0, 0,  0, 0, 0,  0, 8, 1});
-        init_row(8, {0, 9, 0,  0, 0, 0,  0, 0, 0});
+    void init_board_row(
+        int row,
+        std::vector<int> values
+    ) noexcept {
+        for (int col = 0; col < 9; ++col) {
+            int val = values[col];
+            if (val < 0 || val > 9) {
+                LOG("Error: row "<<row<<" column "<<col<<" initialized to: "<<val);
+                exit(1);
+            }
+            set_board(row, col, val);
+        }
+    }
+
+    void set_board(
+        int row,
+        int col,
+        int val
+    ) noexcept {
+        int idx = 9*row + col;
+        board_[idx] = val;
+    }
+
+    int get_board(
+        int row,
+        int col
+    ) noexcept {
+        int idx = 9*row + col;
+        int val = board_[idx];
+        return val;
+    }
+
+    void init_regions() noexcept {
+        regions_.resize(0);
+        init_rows();
+        init_columns();
+        init_boxes();
+    }
+
+    void init_rows() noexcept {
+        for (int row = 0; row < 9; ++row) {
+            init_row(row);
+        }
     }
 
     void init_row(
-        int row,
-        std::vector<int> vec
+        int row
     ) noexcept {
-        int count[10];
-        for (int i = 0; i < 10; ++i) {
-            count[i] = 0;
-        }
+        Region rgn;
+
+        std::stringstream ss;
+        ss<<"Row "<<row;
+        rgn.name_ = std::move(ss.str());
+
         for (int col = 0; col < 9; ++col) {
-            int x = vec[col];
-            if (x < 0 || x > 9) {
-                LOG("Error: row "<<row<<" column "<<col<<" initialized to: "<<x);
-                exit(1);
-            }
-            board_[row][col] = x;
-            ++count[x];
+            int idx = 9*row + col;
+            rgn.cells[col] = idx;
         }
-        int unused[9];
-        int idx = 0;
-        for (int i = 1; i <= 9; ++i) {
-            int c = count[i];
-            if (c > 1) {
-                LOG("Error: row "<<row<<" value "<<i<<" initialized "<<c<<" times.");
-                exit(1);
-            }
-            if (c == 0) {
-                unused[idx] = i;
-                ++idx;
-            }
-        }
-        idx = 0;
+        regions_.push_back(rgn);
+    }
+
+    void init_columns() noexcept {
         for (int col = 0; col < 9; ++col) {
-            if (board_[row][col] == 0) {
-                board_[row][col] = unused[idx];
-                ++idx;
+            init_column(col);
+        }
+    }
+
+    void init_column(
+        int col
+    ) noexcept {
+        Region rgn;
+
+        std::stringstream ss;
+        ss<<"Column "<<col;
+        rgn.name_ = std::move(ss.str());
+
+        for (int row = 0; row < 9; ++row) {
+            int idx= 9*row + col;
+            rgn.cells[row] = idx;
+        }
+        regions_.push_back(rgn);
+    }
+
+    void init_boxes() noexcept {
+        for (int y = 0; y < 3; ++y) {
+            for (int x = 0; x < 3; ++x) {
+                init_box(x, y);
             }
         }
-        for (int i = 0; i < 9; ++i) {
-            if (vec[i] != 0) {
-                continue;
+    }
+
+    void init_box(
+        int x,
+        int y
+    ) noexcept {
+        Region rgn;
+
+        std::stringstream ss;
+        ss<<"Box "<<x<<","<<y;
+        rgn.name_ = std::move(ss.str());
+
+        int m = 0;
+        for (int i = 0; i < 3; ++i) {
+            int row = 3*y + i;
+            for (int k = 0; k < 3; ++k) {
+                int col = 3*x + k;
+                int idx = 9*row + col;
+                rgn.cells[m++] = idx;
             }
-            for (int k = i+1; k < 9; ++k) {
-                if (vec[k] != 0) {
-                    continue;
+        }
+    }
+
+    void check_regions() noexcept {
+        bool error = false;
+        for (auto rgn : regions_) {
+            int used[10];
+            for (int i = 0; i < 10; ++i) {
+                used[i] = 0;
+            }
+            for (int i = 0; i < 9; ++i) {
+                int idx = rgn.cells[i];
+                int val = board_[idx];
+                ++used[val];
+            }
+            for (int i = 1; i <= 9; ++i) {
+                if (used[i] > 1) {
+                    LOG("Error: "<<i<<" used more than once in "<<rgn.name_<<".");
+                    error = true;
                 }
-                moves_.push_back({row, i, k});
             }
+        }
+        if (error) {
+            exit(1);
         }
     }
 
@@ -219,86 +237,10 @@ public:
             if (col == 9) {
                 break;
             }
-            std::cout<<board_[row][col]<<" ";
+            int val = get_board(row, col);
+            std::cout<<val<<" ";
         }
         std::cout<<std::endl;
-    }
-
-    double evaluate() noexcept {
-        int cost = 0;
-        int count[10];
-        count[0] = 0;
-        for (int col = 0; col < 9; ++col) {
-            for (int i = 1; i <= 9; ++i) {
-                count[i] = 0;
-            }
-            for (int row = 0; row < 9; ++row) {
-                int x = board_[row][col];
-                ++count[x];
-            }
-            for (int i = 1; i <= 9; ++i) {
-                if (count[i] == 0) {
-                    ++cost;
-                }
-            }
-        }
-        for (int block_row = 0; block_row < 9; block_row += 3) {
-            for (int block_col = 0; block_col < 9; block_col += 3) {
-                for (int i = 1; i <= 9; ++i) {
-                    count[i] = 0;
-                }
-                for (int row = 0; row < 3; ++row) {
-                    int r = block_row + row;
-                    for (int col = 0; col < 3; ++col) {
-                        int c = block_col + col;
-                        int x = board_[r][c];
-                        ++count[x];
-                    }
-                }
-                for (int i = 1; i <= 9; ++i) {
-                    if (count[i] == 0) {
-                        ++cost;
-                    }
-                }
-            }
-        }
-
-        /**
-        what's the worst possible cost?
-        123|456|789
-        123|456|789
-        123|456|789
-        ---+---+---
-        123|456|789
-        123|456|789
-        123|456|789
-        ---+---+---
-        123|456|789
-        123|456|789
-        123|456|789
-        every column has cost 8.
-        every block has cost 6.
-        so 8*9 + 6*9 = 126
-        **/
-        double scaled = double(cost) / 126.0;
-        return scaled;
-    }
-
-    int choose_move() noexcept {
-        int nmoves = moves_.size();
-        for(;;) {
-            int x = 1 + std::rand()/((RAND_MAX + 1u)/nmoves);
-            if (x < nmoves) {
-                return x;
-            }
-        }
-    }
-
-    void swap_move(
-        int idx
-    ) noexcept {
-        auto& move = moves_[idx];
-        std::swap(board_[move.row_][move.col1_], board_[move.row_][move.col2_]);
     }
 };
 
